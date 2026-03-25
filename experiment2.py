dc6da6cc32f2c51949adb12be3f47b778108d03b
Jonas Loos <33965649+JonasLoos@users.noreply.github.com>
2026-03-25 18:26:52 +0100

    Revert "rename"
    This reverts commit fdf2870a98d6620bd154c0ac16b43bc0a423ed54.

--- experiment2.py
+++ experiment2.py
@@ -20,7 +20,7 @@ import json
 
 from tqdm import trange
 
-from old_utils import (
+from utils import (
     WORDS, Message, neighbors, parse_answer, run_generation,
     print_table, load_model, validate_words,
 )
